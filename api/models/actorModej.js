b49a1b8b295cfd03e4e5f459bd3756f8df6c5942
lola <lolagomezjemes@gmail.com>
2021-02-28 13:40:39 +0100

    Actor Model

---
+++ api/models/actorModej.js
@@ -0,0 +1,57 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema= mongoose.Schema
+
+var actorSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Kindly enter the actor name'
+    },
+    surname: {
+        type: String,
+        required: 'Kindly enter the actor surname'
+    },
+    email: {
+        type: String,
+        required: 'Kindly enter the actor email'
+    },
+    password: {
+        type: String,
+        required: 'Kindly enter the actor password'
+    },
+    address: {
+        type: String,
+        required: 'Kindly enter the actor address'
+    },
+    preferedLanguage: {
+        type: String,
+        required: 'Kindly enter the actor prefered Language',
+        default: 'en'
+    },
+    phone:{
+        type: String,
+        required: 'Kindly enter the actor prefered Language',
+    },
+    photo:{
+        type: Buffer,
+        contentTyoe: String
+    },
+    role: {
+        type: String,
+        required: 'Kindly enter the user role(s)',
+        enum: ['CUSTOMER', 'CLERK', 'ADMINISTRATOR']
+    }, 
+    validated:{
+        type: Boolean,
+        default: false
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    }
+
+    
+}, {stric: false})
+
+
+module.exports = mongoose.model('Actors', ActorSchema);
